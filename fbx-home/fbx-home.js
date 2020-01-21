f63c56e643d2f59c3003990e36a644730fc01a1f
Kevin Maarek <kmaarek@freebox.fr>
2020-01-21 14:14:14 +0100

    Added freebox home requests templates.

---
+++ fbx-home/fbx-home.js
@@ -0,0 +1,67 @@
+let request = require('./../network/request')
+
+var auth = {
+	challenge: null,
+	session: null
+}
+
+module.exports.nodeList = function(filter, callback) {
+	if(auth.session) {
+		let url = 'http://mafreebox.freebox.fr/api/v6/home/nodes'
+		authRequest('GET', url, {}, (statusCode, body) => {
+			console.log(body)
+			let list = []
+			callback(list)
+		})
+	} else {
+		console.log('No session running')
+	}
+}
+
+module.exports.nodeStatus = function(id, callback) {
+	let status = 0
+	callback(status)
+}
+
+module.exports.activateMainAlarm = function(callback) {
+	let success = true
+	callback(success)
+}
+
+module.exports.activateSecondaryAlarm = function(callback) {
+	let success = true
+	callback(success)
+}
+
+module.exports.deactivateAlarm = function(callback) {
+	let success = true
+	callback(success)
+}
+
+module.exports.alarmState = function(callback) {
+	let state = 3
+	callback(state)
+}
+
+module.exports.alarmTargetState = function(callback) {
+	let state = 3
+	callback(state)
+}
+
+// Will be called by every auth request that will need to update auth values.
+function updateAuth(new_session, new_challenge) {
+	if (auth.session != new_session) {
+		console.log("[-] current session  : "+new_session)
+		auth.session = new_session
+		auth.challenge = new_challenge
+	}
+}
+
+function authRequest(method, url, body, callback) {
+	if(auth.challenge != null && auth.session != null) {
+		console.log('Requesting with '+auth.session)
+		request.freeboxRequest(method, url, body, auth, callback, updateAuth)
+	}
+}
+
+module.exports.updateAuth = updateAuth
