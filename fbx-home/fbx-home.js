fa0450ce9ea6785fe61e75bed0ed874419f2dc66
Kevin Maarek <kmaarek@freebox.fr>
2020-01-22 12:12:02 +0100

    Added local values store for node list + implemented alarm functions in fbx home controller.

--- fbx-home/fbx-home.js
+++ fbx-home/fbx-home.js
@@ -1,5 +1,7 @@
 let request = require('./../network/request')
 let nodes = require('./nodes')
+let sensors = require('./sensors')
+let alarm = require('./alarm')
 
 var auth = {
 	challenge: null,
@@ -8,52 +10,193 @@ var auth = {
 	token: null
 }
 
+var local_node_list = null
+var local_alarm_target = null
+
 module.exports.getNodeList = function(filter, callback) {
 	if(auth.session) {
 		let url = 'http://mafreebox.freebox.fr/api/v6/home/nodes'
 		authRequest('GET', url, null, (statusCode, body) => {
 			if(body.success == true) {
 				let list = nodes.nodeList(body, filter)
+				local_node_list = list
 				callback(list)
 			} else {
 				console.log(body)
-				callback([])
+				callback(null)
 			}
 		})
 	} else {
 		console.log('No session running')
-		callback([])
+		callback(null)
+	}
+}
+
+module.exports.getNode = function(id, callback) {
+	if(local_node_list != null) {
+		let node = nodes.retrieveNode(id, local_node_list)
+		if(node != null) {
+			callback(node)
+			return
+		}
+	}
+	if(auth.session) {
+		let url = 'http://mafreebox.freebox.fr/api/v6/home/nodes/'+id
+		authRequest('GET', url, null, (statusCode, body) => {
+			if(body.success == true) {
+				let node = nodes.node(body)
+				callback(node)
+			} else {
+				console.log(body)
+				callback(null)
+			}
+		})
+	} else {
+		console.log('No session running')
+		callback(null)
 	}
 }
 
 module.exports.nodeStatus = function(id, callback) {
-	let status = 0
-	callback(status)
+	if(auth.session) {
+		this.getNode(id, (node) => {
+			let url = 'http://mafreebox.freebox.fr/api/v6/home/endpoints/'+id+'/'+node.statusId
+			authRequest('GET', url, null, (statusCode, body) => {
+				if(body.success == true) {
+					let status = sensors.sensorStatus(body)
+					callback(status)
+				} else {
+					console.log(body)
+					callback(null)
+				}
+			})
+		})
+	} else {
+		console.log('No session running')
+		callback(null)
+	}
+}
+
+module.exports.getAlarm = function(callback) {
+	if(auth.session) {
+		let alarm_node = null
+		if(local_node_list != null) {
+			alarm_node = nodes.retrieveAlarmNode(local_node_list)
+		}
+		if(alarm_node == null) {
+			this.getNodeList("alarm", (list) => {
+				if(list.length > 0) {
+					callback(list[0])
+				} else {
+					callback(null)
+				}
+			})
+		} else {
+			callback(alarm_node)
+		}
+	} else {
+		console.log('No session running')
+		callback(null)
+	}
+	
 }
 
 module.exports.activateMainAlarm = function(callback) {
-	let success = true
-	callback(success)
+	if(auth.session) {
+		this.getAlarm((alarm_node) => {
+			let ep_id = alarm.mainAlarmRequestId(alarm_node.data)
+			let url = 'http://mafreebox.freebox.fr/api/v6/home/endpoints/'+alarm_node.id+'/'+ep_id
+			let data = {
+				id: alarm_node.id,
+				value: null
+			}
+			local_alarm_target = 1
+			authRequest('PUT', url, data, (statusCode, body) => {
+				if(body.success == true) {
+					callback(true)
+				} else {
+					callback(null)
+				}
+			})
+		})
+	} else {
+		console.log('No session running')
+		callback(null)
+	}
 }
 
 module.exports.activateSecondaryAlarm = function(callback) {
-	let success = true
-	callback(success)
+	if(auth.session) {
+		this.getAlarm((alarm_node) => {
+			let ep_id = alarm.secondaryAlarmRequestId(alarm_node.data)
+			let url = 'http://mafreebox.freebox.fr/api/v6/home/endpoints/'+alarm_node.id+'/'+ep_id
+			let data = {
+				id: alarm_node.id,
+				value: null
+			}
+			local_alarm_target = 2
+			authRequest('PUT', url, data, (statusCode, body) => {
+				if(body.success == true) {
+					callback(true)
+				} else {
+					callback(null)
+				}
+			})
+		})
+	} else {
+		console.log('No session running')
+		callback(null)
+	}
 }
 
 module.exports.deactivateAlarm = function(callback) {
-	let success = true
-	callback(success)
+	if(auth.session) {
+		this.getAlarm((alarm_node) => {
+			let ep_id = alarm.alarmOffId(alarm_node.data)
+			let url = 'http://mafreebox.freebox.fr/api/v6/home/endpoints/'+alarm_node.id+'/'+ep_id
+			let data = {
+				id: alarm_node.id,
+				value: null
+			}
+			local_alarm_target = 0
+			authRequest('PUT', url, data, (statusCode, body) => {
+				if(body.success == true) {
+					callback(true)
+				} else {
+					console.log(body)
+					callback(null)
+				}
+			})
+		})
+	} else {
+		console.log('No session running')
+		callback(null)
+	}
 }
 
 module.exports.alarmState = function(callback) {
-	let state = 3
-	callback(state)
+	if(auth.session) {
+		this.getAlarm((alarm_node) => {
+			let ep_id = alarm.alarmStateId(alarm_node.data)
+			let url = 'http://mafreebox.freebox.fr/api/v6/home/endpoints/'+alarm_node.id+'/'+ep_id
+			authRequest('GET', url, null, (statusCode, body) => {
+				console.log(body)
+				if(body.success == true) {
+					callback(body.result.value)
+				} else {
+					console.log(body)
+					callback(null)
+				}
+			})
+		})
+	} else {
+		console.log('No session running')
+		callback(null)
+	}
 }
 
 module.exports.alarmTargetState = function(callback) {
-	let state = 3
-	callback(state)
+	callback(local_alarm_target)
 }
 
 // Will be called by every auth request that will need to update auth values.
