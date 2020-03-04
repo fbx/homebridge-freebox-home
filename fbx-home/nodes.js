a0f263492b8de3d2db6362f3a0ba60a7e9d1f593
Kevin Maarek <kmaarek@freebox.fr>
2020-03-04 15:10:22 +0100

    Added better filter check for node list.

--- fbx-home/nodes.js
+++ fbx-home/nodes.js
@@ -6,24 +6,37 @@ module.exports.nodeList = function(object, filter) {
 	if(object.success == true) {
 		var items = []
 		for(node of object.result) {
-			var endpointForStatus = null
-			for(endpoint of node.show_endpoints) {
-				if(endpoint.name == 'trigger') {
-					endpointForStatus = endpoint.id
-				}
-			}
-			let item = {
-				id: node.id,
-				type: node.category,
-				label: getObjectLabel(node, items),
-				statusId: endpointForStatus,
-				data: node
-			}
 			if(filter != null) {
-				if(item.type == filter) {
+				if(node.category == filter) {
+					var endpointForStatus = null
+					for(endpoint of node.show_endpoints) {
+						if(endpoint.name == 'trigger') {
+							endpointForStatus = endpoint.id
+						}
+					}
+					let item = {
+						id: node.id,
+						type: node.category,
+						label: getObjectLabel(node, items),
+						statusId: endpointForStatus,
+						data: node
+					}
 					items.push(item)
 				}
 			} else {
+				var endpointForStatus = null
+				for(endpoint of node.show_endpoints) {
+					if(endpoint.name == 'trigger') {
+						endpointForStatus = endpoint.id
+					}
+				}
+				let item = {
+					id: node.id,
+					type: node.category,
+					label: getObjectLabel(node, items),
+					statusId: endpointForStatus,
+					data: node
+				}
 				items.push(item)
 			}
 		}
